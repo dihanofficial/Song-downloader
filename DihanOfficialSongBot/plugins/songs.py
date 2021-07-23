23d977ee0143de339f2b6084cccb38543aae6b4a
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-23 07:23:21 +0530

    Update songs.py

--- DihanOfficialSongBot/plugins/songs.py
+++ DihanOfficialSongBot/plugins/songs.py
@@ -5,7 +5,7 @@ import requests
 import aiohttp
 import youtube_dl
 
-from DihanOfficialSongBot import Dihansongbot as app
+from DihanOfficialSongBot import DihanOfficialSongBot  as app
 from pyrogram import filters, Client
 from youtube_search import YoutubeSearch
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
@@ -53,7 +53,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**~ Uploaded by @dihan_official ~**'
+        rep = '** Uploaded By @DihanOfficial **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
